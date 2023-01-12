cf279bc3bcd8bf62773c8a4bc2f51efc5132a1f5
Victor <victorshiira4869@gmail.com>
2023-01-12 15:21:03 -0300

    Create function to call an operation on 2 numbers based on the operator selected

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,3 +13,13 @@ function multiplication(num1, num2) {
 function division(num1, num2) {
   return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+  return operator === "+"
+    ? addition(num1, num2)
+    : operator === "-"
+    ? subtraction(num1, num2)
+    : operator === "*"
+    ? multiplication(num1, num2)
+    : division(num1, num2);
+}
