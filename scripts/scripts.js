06edf5601a348228790034667674eac079f231c9
Victor <victorshiira4869@gmail.com>
2023-01-13 14:58:56 -0300

    Display the operations result on clicking the equals button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,14 @@
 
 const result = document.querySelector(".result");
 const valueButtons = document.querySelectorAll(".value-button");
+const operatorButtons = document.querySelectorAll(".operation");
+const equalsButton = document.querySelector(".equals");
+
+let prevValue = 0;
+let currentValue = 0;
+let isDoingAnOperation = false;
+let currentOperator = "";
+let clickedOnce = false;
 
 //Functions --------------------------------------------------------------
 
@@ -22,27 +30,55 @@ function division(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
-  return operator === "+"
-    ? addition(num1, num2)
-    : operator === "-"
+  return operator === "-"
     ? subtraction(num1, num2)
+    : operator === "/"
+    ? division(num1, num2)
     : operator === "*"
     ? multiplication(num1, num2)
-    : division(num1, num2);
+    : addition(num1, num2);
+}
+
+function displayResult() {
+  result.textContent = operate(currentOperator, prevValue, currentValue);
 }
 
 //Events -----------------------------------------------------------------
 
 valueButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (result.textContent === "0") {
+    if (result.textContent === "0" || isDoingAnOperation) {
+      prevValue = Number(result.textContent);
+
       result.textContent = button.textContent;
+      currentValue = Number(result.textContent);
+
+      isDoingAnOperation = false;
     } else {
       if (
         !(button.textContent === "." && result.textContent.indexOf(".") > -1)
       ) {
         result.textContent += button.textContent;
+
+        currentValue = Number(result.textContent);
       }
     }
   });
 });
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (!isDoingAnOperation) {
+      if (clickedOnce) {
+        displayResult();
+      }
+
+      currentOperator = button.textContent;
+
+      isDoingAnOperation = true;
+      clickedOnce = true;
+    }
+  });
+});
+
+equalsButton.addEventListener("click", displayResult);
